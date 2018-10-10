5bffde691a41eb9f902161ecca840ee3ab5288a6
Felipe Rdguez Arias <farias2014@nauta.cu>
2018-10-10 15:53:51 -0400

    sinfuncionar el security.yml

--- src/NB/CommonBundle/Util/Entity.php
+++ src/NB/CommonBundle/Util/Entity.php
@@ -12,6 +12,21 @@ namespace NB\CommonBundle\Util;
 class Entity {
 
     const ADMINISTRADOR = "CommonBundle:Administrador";
+    const ASIGNACION = "CommonBundle:Asignacion";
+    const EQUIPO = "CommonBundle:Equipo";
+    const ESPECIALIDAD = "CommonBundle:Especialidad";
+    const INDICACION = "CommonBundle:Indicacion";
+    const PACIENTE = "CommonBundle:Paciente";
+    const PLANIFICACION = "CommonBundle:Planificacion";
+    const PROCEDENCIA = "CommonBundle:Procedencia";
+    const PRUEBA = "CommonBundle:Prueba";
+    const SOLICITUD = "CommonBundle:Solicitud";
+    const TURNO = "CommonBundle:Turno";
+    const TURNO_INDICACION = "CommonBundle:TurnoIndicacion";
     const USUARIO = "CommonBundle:Usuario";
+
+    const DOCUMENTO = "MediaBundle:Documento";
+    const MEDIA = "MediaBundle:Media";
+    const TIPO_MEDIA = "MediaBundle:TipoMedia";
 
 }
