de0086d2ee3c45c63715d539dda5bee342decee8
raidel <raidel.rodriguez@uo.edu.cu>
2018-10-29 11:59:57 -0400

    Hacer CRUD de nomenclador Equipo

---
+++ src/NB/CommonBundle/Form/EquipoType.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace NB\CommonBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class EquipoType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('nombre')->add('descripcion',null, array('label'=>"Descripción"));
+    }/**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'NB\CommonBundle\Entity\Equipo'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'nb_commonbundle_equipo';
+    }
+
+
+}
